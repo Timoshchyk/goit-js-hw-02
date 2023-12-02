c8a6ba2d4712a4d0c408b3a9e15fde750d69124d
Timoshchyk <timosuko202@gmail.com>
2023-12-02 17:00:45 +0200

    homework-2

---
+++ js/task-4.js
@@ -0,0 +1,32 @@
+function getShippingCost(country) {
+    let message;
+    switch (key) {
+        case "China" :
+            message = `Shipping to ${country} will cost 100 credits`;
+            return message;
+            break;
+        case "Chile" :
+            message = `Shipping to ${country} will cost 250 credits`;
+        return message;
+            break;
+        case "Australia" :
+            message = `Shipping to ${country} will cost 170 credits`;
+        return message;
+            break;
+        case "Jamaica" :
+            message = `Shipping to ${country} will cost 170 credits`;
+        return message;
+            break;
+        default:
+            message = "Sorry, there is no delivery to your country";
+        return message;
+            break;
+    }
+}
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
